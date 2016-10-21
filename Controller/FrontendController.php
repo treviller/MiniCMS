2dcb8b0e5cdc341675794ad6a716d680f8c91983
TREVILLER <tanguy.reviller@gmail.com>
2016-10-21 09:00:46 +0200

    Creating doctrine entity (page) and associated routing

--- Controller/FrontendController.php
+++ Controller/FrontendController.php
@@ -5,8 +5,29 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class FrontendController extends Controller
 {
+	/**
+	 * 'frontend_home' '/'
+	 * @return \Symfony\Component\HttpFoundation\Response
+	 */
 	public function homeAction()
 	{
 		return $this->render('MiniCMSBundle:Frontend:home.html.twig');
+	}
+
+	/**
+	 * 'frontend_view', '/page/{title}'
+	 * @param string $title
+	 * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+	 */
+	public function viewAction($title)
+	{
+		$view = null;
+
+		if($view === null)
+		{
+			return $this->redirectToRoute('frontend_home');
+		}
+
+		return $this->render($view);
 	}
 }
---
+++ Entity/Page.php
@@ -0,0 +1,129 @@
+<?php
+namespace MiniCMSBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+
+/**
+ * Page
+ *
+ * @ORM\Table(name="pages")
+ * @ORM\Entity(repositoryClass="MiniCMSBundle\Repository\PageRepository")
+ */
+class Page
+{
+	/**
+	 * @var int
+	 *
+	 * @ORM\Column(name="id", type="integer")
+	 * @ORM\Id
+	 * @ORM\GeneratedValue(strategy="AUTO")
+	 */
+	private $id;
+
+	/**
+	 * @var string
+	 *
+	 * @ORM\Column(name="title", type="string", length=255)
+	 */
+	private $title;
+
+	/**
+	 * @var string
+	 *
+	 * @ORM\Column(name="contenu", type="text")
+	 */
+	private $content;
+
+	/**
+	 * @var string
+	 *
+	 * @Gedmo\Slug(fields={"title"})
+	 * @ORM\Column(name="slug", type="string", length=255, unique=true)
+	 */
+	private $slug;
+
+
+	/**
+	 * Get id
+	 *
+	 * @return int
+	 */
+	public function getId()
+	{
+		return $this->id;
+	}
+
+	/**
+	 * Set title
+	 *
+	 * @param string $title
+	 *
+	 * @return Page
+	 */
+	public function setTitle($title)
+	{
+		$this->title = $title;
+
+		return $this;
+	}
+
+	/**
+	 * Get title
+	 *
+	 * @return string
+	 */
+	public function getTitle()
+	{
+		return $this->title;
+	}
+
+	/**
+	 * Set contenu
+	 *
+	 * @param string $contenu
+	 *
+	 * @return Page
+	 */
+	public function setContent($content)
+	{
+		$this->content = $content;
+
+		return $this;
+	}
+
+	/**
+	 * Get contenu
+	 *
+	 * @return string
+	 */
+	public function getContent()
+	{
+		return $this->content;
+	}
+
+	/**
+	 * Set slug
+	 *
+	 * @param string $slug
+	 *
+	 * @return Page
+	 */
+	public function setSlug($slug)
+	{
+		$this->slug = $slug;
+
+		return $this;
+	}
+
+	/**
+	 * Get slug
+	 *
+	 * @return string
+	 */
+	public function getSlug()
+	{
+		return $this->slug;
+	}
+}
+
